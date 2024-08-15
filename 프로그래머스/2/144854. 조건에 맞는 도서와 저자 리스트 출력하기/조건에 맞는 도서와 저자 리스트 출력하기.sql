15cdd13badc7b62700a596aa21e3f050eaf14cf8
Seonghun, Bae <129490755+baehoonbae@users.noreply.github.com>
2024-08-15 23:04:20 +0900

    [level 2] Title: 조건에 맞는 도서와 저자 리스트 출력하기, Time: 0.00 ms, Memory: 0.0 MB -BaekjoonHub

---
+++ 프로그래머스/2/144854. 조건에 맞는 도서와 저자 리스트 출력하기/조건에 맞는 도서와 저자 리스트 출력하기.sql
@@ -0,0 +1,14 @@
+SELECT
+    book_id,
+    author_name,
+    date_format(published_date, "%Y-%m-%d") as published_date
+from
+    book b
+join
+    author a
+on
+    b.author_id=a.author_id
+where
+    category = "경제"
+order by
+    published_date asc;
