d22aef16fdfd1322f1e9aba05af5243036e09922
Seonghun, Bae <129490755+baehoonbae@users.noreply.github.com>
2024-09-24 00:39:58 +0900

    [Platinum IV] Title: 님 게임 2, Time: 68 ms, Memory: 11708 KB -BaekjoonHub

---
+++ 백준/Platinum/11868. 님 게임 2/님 게임 2.java
@@ -0,0 +1,22 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args)throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st;
+        int n = Integer.parseInt(br.readLine());
+        int mask = 0;
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            mask ^= Integer.parseInt(st.nextToken());
+        }
+        if (mask > 0) {
+            System.out.println("koosaga");
+        } else {
+            System.out.println("cubelover");
+        }
+    }
+}
