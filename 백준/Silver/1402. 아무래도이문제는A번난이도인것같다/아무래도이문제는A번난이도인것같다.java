4f1b51414e7af75aa2da583e0ecef5a9d6fc8b7b
Seonghun, Bae <129490755+baehoonbae@users.noreply.github.com>
2024-10-06 04:07:06 +0900

    [Silver V] Title: 아무래도이문제는A번난이도인것같다, Time: 104 ms, Memory: 13112 KB -BaekjoonHub

---
+++ 백준/Silver/1402. 아무래도이문제는A번난이도인것같다/아무래도이문제는A번난이도인것같다.java
@@ -0,0 +1,14 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int n=sc.nextInt();
+        sc.nextLine();
+        for(int t=0;t<n;t++){
+            int a = sc.nextInt();
+            int b=sc.nextInt();
+            System.out.println("yes");
+        }
+    }
+}
