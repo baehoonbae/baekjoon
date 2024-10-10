c830ca1c45fed835f53d4fb1c40c63ea1449e094
Seonghun, Bae <129490755+baehoonbae@users.noreply.github.com>
2024-10-10 22:47:12 +0900

    [Gold I] Title: GCD(n, k) = 1, Time: 108 ms, Memory: 13028 KB -BaekjoonHub

--- 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.java
+++ 백준/Gold/11689. GCD（n， k） ＝ 1/GCD（n， k） ＝ 1.java
@@ -1,35 +1,15 @@
 import java.util.Scanner;
 
 public class Main {
-    static final long max=(long)Math.pow(10L,12);
-    static boolean[] che=new boolean[1000001];
-
     public static void main(String[] args) {
-        pre();
         long n=new Scanner(System.in).nextLong();
-        long origin=n;
-
-        long res=n;
+        long origin=n,res=n;
         for(long i=2;i*i<=origin;i++){
             if(n%i==0){
-                while(n%i==0){
-                    n/=i;
-                }
+                while(n%i==0)n/=i;
                 res-=res/i;
             }
         }
-
-        if(n>1)res-=res/n;
-
-        System.out.println(res);
-    }
-
-    private static void pre(){
-        for(long i=2;i<=1000000;i++){
-            if(che[(int)i])continue;
-            for(long j=i*2;j<=1000000;j+=i){
-                che[(int)j]=true;
-            }
-        }
+        System.out.println(n>1?res-(res/n):res);
     }
 }
