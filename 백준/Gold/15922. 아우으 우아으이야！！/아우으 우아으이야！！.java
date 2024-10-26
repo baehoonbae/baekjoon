5b30da286a322ba7d02985361c27580c0b4d7ddf
Seonghun, Bae <129490755+baehoonbae@users.noreply.github.com>
2024-10-26 19:04:26 +0900

    [Gold V] Title: 아우으 우아으이야!!, Time: 128 ms, Memory: 15404 KB -BaekjoonHub

--- 백준/Gold/15922. 아우으 우아으이야！！/아우으 우아으이야！！.java
+++ 백준/Gold/15922. 아우으 우아으이야！！/아우으 우아으이야！！.java
@@ -1,29 +1,32 @@
-import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.util.Arrays;
 
-public class Main {
-    static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
-    static int n;
-    static Line[] arr;
-
-    static class Line{
-        int start,end;
-        Line(int start,int end){this.start=start;this.end=end;}
+class Line implements Comparable<Line>{
+    int start,end;
+    Line(int start,int end){this.start=start;this.end=end;}
+    @Override
+    public int compareTo(Line o){
+        if(this.start==o.start)return o.end-this.end;
+        return this.start-o.start;
     }
+}
 
+public class Main extends FastInput {
     public static void main(String[] args)throws IOException {
-        n=Integer.parseInt(br.readLine());
-        arr=new Line[n];
+        initFI();
+        int n=nextInt();
+
+        Line[] arr=new Line[n];
         for(int i=0;i<n;i++){
-            st=new StringTokenizer(br.readLine());
-            arr[i]=new Line(Integer.parseInt(st.nextToken()),Integer.parseInt(st.nextToken()));
+            arr[i]=new Line(nextInt(),nextInt());
         }
+        Arrays.sort(arr);
+
         long sum=0;
         int start=arr[0].start;
         int end=arr[0].end;
+
         for(int i=1;i<n;i++){
             if(arr[i].start>end){
                 sum+=end-start;
@@ -31,7 +34,42 @@ public class Main {
             }
             if(arr[i].end>end)end=arr[i].end;
         }
+
         sum+=end-start;
         System.out.println(sum);
+    }
+}
+
+class FastInput {
+    private static final int DEFAULT_BUFFER_SIZE = 1 << 16;
+    private static DataInputStream inputStream;
+    private static byte[] buffer;
+    private static int curIdx, maxIdx;
+
+    protected static void initFI() {
+        inputStream = new DataInputStream(System.in);
+        buffer = new byte[DEFAULT_BUFFER_SIZE];
+        curIdx = maxIdx = 0;
+    }
+
+    protected static int nextInt() throws IOException {
+        int ret = 0;
+        byte c = read();
+        while (c <= ' ') c = read();
+        boolean neg = (c == '-');
+        if (neg) c = read();
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+        if (neg) return -ret;
+        return ret;
+    }
+
+    private static byte read() throws IOException {
+        if (curIdx == maxIdx) {
+            maxIdx = inputStream.read(buffer, curIdx = 0, DEFAULT_BUFFER_SIZE);
+            if (maxIdx == -1) buffer[0] = -1;
+        }
+        return buffer[curIdx++];
     }
 }
