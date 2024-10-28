cb6a8f7fa96c7326a4b3f39332c9bb143fcd2b94
Seonghun, Bae <129490755+baehoonbae@users.noreply.github.com>
2024-10-28 22:54:02 +0900

    [Silver III] Title: 한번 쏘면 멈출 수 없어, Time: 72 ms, Memory: 11504 KB -BaekjoonHub

---
+++ 백준/Silver/3991. 한번 쏘면 멈출 수 없어/한번 쏘면 멈출 수 없어.java
@@ -0,0 +1,118 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+
+class Main extends FI1 {
+    public static void main(String[] args)throws IOException {
+        initFI();
+        int[] dy={0,1,0,-1};
+        int[] dx={1,0,-1,0};
+
+        int n=nextInt();
+        int m=nextInt();
+        int k=nextInt();
+        int[][] arr=new int[n][m];
+        int[] balls=new int[k+1];
+        for(int i=1;i<=k;i++){
+            balls[i]=nextInt();
+        }
+        int y=0,x=0,d=0,idx=1;
+        arr[y][x]=idx;
+        balls[idx]--;
+        while(idx<=k){
+            int ny=y+dy[d];
+            int nx=x+dx[d];
+            if(balls[idx]==0)idx++;
+
+            if(ny<0||nx<0||ny>=n||nx>=m||arr[ny][nx]!=0){
+                d=(d+1)%4;
+                continue;
+            }
+
+
+            arr[ny][nx]=idx;
+            balls[idx]--;
+            y=ny;
+            x=nx;
+        }
+        for(int i=0;i<n;i++){
+            for(int j=0;j<m;j++){
+                System.out.print(arr[i][j]);
+            }
+            System.out.println();
+        }
+    }
+}
+
+class FI1 {
+    private static final int DEFAULT_BUFFER_SIZE = 1 << 16;
+    private static DataInputStream inputStream;
+    private static byte[] buffer;
+    private static int curIdx, maxIdx;
+
+    protected static void initFI() {
+        inputStream = new DataInputStream(System.in);
+        buffer = new byte[DEFAULT_BUFFER_SIZE];
+        curIdx = maxIdx = 0;
+    }
+
+    protected static int nextInt() throws IOException {
+        int ret = 0;
+        byte c = read();
+        while (c <= ' ') c = read();
+        boolean neg = (c == '-');
+        if (neg) c = read();
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+        if (neg) return -ret;
+        return ret;
+    }
+
+    protected static double nextDouble() throws IOException {
+        double ret = 0, div = 1;
+        byte c = read();
+        while (c <= ' ') c = read();
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+        if (c == '.') while ((c = read()) >= '0' && c <= '9')
+            ret += (c - '0') / (div *= 10);
+        return ret;
+    }
+
+    protected static long nextLong() throws IOException {
+        long ret = 0;
+        byte c = read();
+        while (c <= ' ') c = read();
+        boolean neg = (c == '-');
+        if (neg) c = read();
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+        if (neg) return -ret;
+        return ret;
+    }
+
+    protected static String next() throws IOException {
+        StringBuilder sb = new StringBuilder();
+        byte c = read();
+
+        // 공백 문자는 무시하고 다음 문자를 읽음
+        while (c <= ' ') c = read();
+
+        // 공백이 아닌 문자들을 읽어 문자열 생성
+        while (c > ' ') {
+            sb.append((char) c);
+            c = read();
+        }
+        return sb.toString();
+    }
+
+    private static byte read() throws IOException {
+        if (curIdx == maxIdx) {
+            maxIdx = inputStream.read(buffer, curIdx = 0, DEFAULT_BUFFER_SIZE);
+            if (maxIdx == -1) buffer[0] = -1;
+        }
+        return buffer[curIdx++];
+    }
+}
