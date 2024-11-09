a715aed7c82ac0c65bd62972d3dd2c769aebfe3b
Seonghun, Bae <129490755+baehoonbae@users.noreply.github.com>
2024-11-10 03:34:43 +0900

    [Gold IV] Title: 전력난, Time: 348 ms, Memory: 49732 KB -BaekjoonHub

---
+++ 백준/Gold/6497. 전력난/전력난.java
@@ -0,0 +1,149 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+import java.util.PriorityQueue;
+
+// 최th스패닝트리
+public class Main extends FI1 {
+    static int n,m;
+    static int[] parent;
+
+    public static void main(String[] args) throws IOException {
+        initFI();
+        while(true){
+            n=nextInt();
+            m=nextInt();
+            
+            if(n==0&&m==0)return;
+
+            parent=new int[2*n+1];
+            for(int i=1;i<=n;i++){
+                parent[i]=i;
+            }
+            PriorityQueue<Edge>pq=new PriorityQueue<>();
+            int total=0;
+            for(int i=0;i<m;i++){
+                int a=nextInt();
+                int b=nextInt();
+                int c=nextInt();
+                pq.add(new Edge(a,b,c));
+                total+=c;
+            }
+
+            int answer=0;
+            int ecnt=0;
+            while(!pq.isEmpty()&&ecnt!=n-1){
+                Edge cur=pq.poll();
+                if(find(cur.from)!=find(cur.to)){
+                    union(cur.from,cur.to);
+                    answer+=cur.cost;
+                    ecnt++;
+                }
+            }
+            System.out.println(total-answer);
+        }
+    }
+
+    private static int find(int x){
+        if(x==parent[x]){
+            return x;
+        }
+        return parent[x]=find(parent[x]);
+    }
+
+    private static void union(int a,int b){
+        a=find(a);
+        b=find(b);
+        if(a==b)return;
+        parent[b]=a;
+    }
+}
+
+
+class Edge implements Comparable<Edge>{
+    int from,to,cost;
+
+    Edge(int from,int to,int cost){
+        this.from=from;
+        this.to=to;
+        this.cost=cost;
+    }
+
+    @Override
+    public int compareTo(Edge o){
+        return Integer.compare(this.cost,o.cost);
+    }
+}
+
+class FI1 {
+    private static final int DEFAULT_BUFFER_SIZE = 1 << 16;
+    private static DataInputStream inputStream;
+    private static byte[] buffer;
+    private static int curIdx, maxIdx;
+
+    protected static void initFI() {
+        inputStream = new DataInputStream(System.in);
+        buffer = new byte[DEFAULT_BUFFER_SIZE];
+        curIdx = maxIdx = 0;
+    }
+
+    protected static int nextInt() throws IOException {
+        int ret = 0;
+        byte c = read();
+        while (c <= ' ') c = read();
+        boolean neg = (c == '-');
+        if (neg) c = read();
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+        if (neg) return -ret;
+        return ret;
+    }
+
+    protected static double nextDouble() throws IOException {
+        double ret = 0, div = 1;
+        byte c = read();
+        while (c <= ' ') c = read();
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+        if (c == '.') while ((c = read()) >= '0' && c <= '9')
+            ret += (c - '0') / (div *= 10);
+        return ret;
+    }
+
+    protected static long nextLong() throws IOException {
+        long ret = 0;
+        byte c = read();
+        while (c <= ' ') c = read();
+        boolean neg = (c == '-');
+        if (neg) c = read();
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+        if (neg) return -ret;
+        return ret;
+    }
+
+    protected static String next() throws IOException {
+        StringBuilder sb = new StringBuilder();
+        byte c = read();
+
+        // 공백 문자는 무시하고 다음 문자를 읽음
+        while (c <= ' ') c = read();
+
+        // 공백이 아닌 문자들을 읽어 문자열 생성
+        while (c > ' ') {
+            sb.append((char) c);
+            c = read();
+        }
+        return sb.toString();
+    }
+
+    private static byte read() throws IOException {
+        if (curIdx == maxIdx) {
+            maxIdx = inputStream.read(buffer, curIdx = 0, DEFAULT_BUFFER_SIZE);
+            if (maxIdx == -1) buffer[0] = -1;
+        }
+        return buffer[curIdx++];
+    }
+}
