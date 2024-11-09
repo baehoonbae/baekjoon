a180c6c3c644ed68ca984b8622ae62d6a8820507
Seonghun, Bae <129490755+baehoonbae@users.noreply.github.com>
2024-11-10 02:19:12 +0900

    [Silver II] Title: 격자상의 경로, Time: 164 ms, Memory: 11548 KB, Score: 32 point -BaekjoonHub

---
+++ 백준/Silver/10164. 격자상의 경로/격자상의 경로.java
@@ -0,0 +1,102 @@
+import java.io.DataInputStream;
+import java.io.IOException;
+
+public class Main extends FI1 {
+    static int n,m,k;
+
+    public static void main(String[] args) throws IOException {
+        initFI();
+        n=nextInt();
+        m=nextInt();
+        k=nextInt();
+        if(k==0){
+            System.out.println(dfs(1,1,n,m));
+        }else{
+            System.out.println(dfs(1,1,(k/m)+1,k%m)*dfs((k/m)+1,k%m,n,m));
+        }
+        }
+
+    private static int dfs(int y,int x,int ey,int ex){
+        if(y>ey||x>ex)return 0;
+        if(y==ey&&x==ex)return 1;
+
+        int res=0;
+        res+=dfs(y+1,x,ey,ex);
+        res+=dfs(y,x+1,ey,ex);
+        return res;
+    }
+}
+
+class FI1 {
+    private static final int DEFAULT_BUFFER_SIZE = 1 << 16;
+    private static DataInputStream inputStream;
+    private static byte[] buffer;
+    private static int curIdx, maxIdx;
+
+    protected static void initFI() {
+        inputStream = new DataInputStream(System.in);
+        buffer = new byte[DEFAULT_BUFFER_SIZE];
+        curIdx = maxIdx = 0;
+    }
+
+    protected static int nextInt() throws IOException {
+        int ret = 0;
+        byte c = read();
+        while (c <= ' ') c = read();
+        boolean neg = (c == '-');
+        if (neg) c = read();
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+        if (neg) return -ret;
+        return ret;
+    }
+
+    protected static double nextDouble() throws IOException {
+        double ret = 0, div = 1;
+        byte c = read();
+        while (c <= ' ') c = read();
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+        if (c == '.') while ((c = read()) >= '0' && c <= '9')
+            ret += (c - '0') / (div *= 10);
+        return ret;
+    }
+
+    protected static long nextLong() throws IOException {
+        long ret = 0;
+        byte c = read();
+        while (c <= ' ') c = read();
+        boolean neg = (c == '-');
+        if (neg) c = read();
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+        if (neg) return -ret;
+        return ret;
+    }
+
+    protected static String next() throws IOException {
+        StringBuilder sb = new StringBuilder();
+        byte c = read();
+
+        // 공백 문자는 무시하고 다음 문자를 읽음
+        while (c <= ' ') c = read();
+
+        // 공백이 아닌 문자들을 읽어 문자열 생성
+        while (c > ' ') {
+            sb.append((char) c);
+            c = read();
+        }
+        return sb.toString();
+    }
+
+    private static byte read() throws IOException {
+        if (curIdx == maxIdx) {
+            maxIdx = inputStream.read(buffer, curIdx = 0, DEFAULT_BUFFER_SIZE);
+            if (maxIdx == -1) buffer[0] = -1;
+        }
+        return buffer[curIdx++];
+    }
+}
