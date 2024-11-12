a0e4c3820fcce87656285947e31f5c1c19e7c083
Seonghun, Bae <129490755+baehoonbae@users.noreply.github.com>
2024-11-12 21:57:03 +0900

    [Platinum IV] Title: 열혈강호, Time: 840 ms, Memory: 44156 KB -BaekjoonHub

--- 백준/Platinum/11375. 열혈강호/열혈강호.java
+++ 백준/Platinum/11375. 열혈강호/열혈강호.java
@@ -1,43 +1,39 @@
-import java.io.BufferedReader;
+import java.io.DataInputStream;
 import java.io.IOException;
-import java.io.InputStreamReader;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
-import java.util.StringTokenizer;
 
-public class Main {
-    static BufferedReader br=new BufferedReader(new InputStreamReader(System.in));
-    static StringTokenizer st;
+public class Main extends FI1 {
     static int n,m;
     static List<Integer>[] adj;
     static int[] matched;
     static boolean[] visited;
 
     public static void main(String[] args) throws IOException {
-        st=new StringTokenizer(br.readLine());
-        n=Integer.parseInt(st.nextToken());
-        m=Integer.parseInt(st.nextToken());
+        initFI();
+        n=nextInt();
+        m=nextInt();
         adj=new List[n+1];
         matched=new int[m+1];
         for(int i=1;i<=n;i++){
             adj[i]=new ArrayList<>();
         }
         Arrays.fill(matched,-1);
+
         for(int i=1;i<=n;i++){
-            st=new StringTokenizer(br.readLine());
-            int num=Integer.parseInt(st.nextToken());
+            int num=nextInt();
             for(int j=0;j<num;j++){
-                int a=Integer.parseInt(st.nextToken());
+                int a=nextInt();
                 adj[i].add(a);
             }
         }
-        int match=0;
+        int matches=0;
         for(int i=1;i<=n;i++){
             visited=new boolean[n+1];
-            if(dfs(i))match++;
+            if(dfs(i))matches++;
         }
-        System.out.println(match);
+        System.out.println(matches);
     }
 
     private static boolean dfs(int cur){
@@ -52,5 +48,80 @@ public class Main {
             }
         }
         return false;
+
+    }
+}
+
+class FI1 {
+    private static final int DEFAULT_BUFFER_SIZE = 1 << 16;
+    private static DataInputStream inputStream;
+    private static byte[] buffer;
+    private static int curIdx, maxIdx;
+
+    protected static void initFI() {
+        inputStream = new DataInputStream(System.in);
+        buffer = new byte[DEFAULT_BUFFER_SIZE];
+        curIdx = maxIdx = 0;
+    }
+
+    protected static int nextInt() throws IOException {
+        int ret = 0;
+        byte c = read();
+        while (c <= ' ') c = read();
+        boolean neg = (c == '-');
+        if (neg) c = read();
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+        if (neg) return -ret;
+        return ret;
+    }
+
+    protected static double nextDouble() throws IOException {
+        double ret = 0, div = 1;
+        byte c = read();
+        while (c <= ' ') c = read();
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+        if (c == '.') while ((c = read()) >= '0' && c <= '9')
+            ret += (c - '0') / (div *= 10);
+        return ret;
+    }
+
+    protected static long nextLong() throws IOException {
+        long ret = 0;
+        byte c = read();
+        while (c <= ' ') c = read();
+        boolean neg = (c == '-');
+        if (neg) c = read();
+        do {
+            ret = ret * 10 + c - '0';
+        } while ((c = read()) >= '0' && c <= '9');
+        if (neg) return -ret;
+        return ret;
+    }
+
+    protected static String next() throws IOException {
+        StringBuilder sb = new StringBuilder();
+        byte c = read();
+
+        // 공백 문자는 무시하고 다음 문자를 읽음
+        while (c <= ' ') c = read();
+
+        // 공백이 아닌 문자들을 읽어 문자열 생성
+        while (c > ' ') {
+            sb.append((char) c);
+            c = read();
+        }
+        return sb.toString();
+    }
+
+    private static byte read() throws IOException {
+        if (curIdx == maxIdx) {
+            maxIdx = inputStream.read(buffer, curIdx = 0, DEFAULT_BUFFER_SIZE);
+            if (maxIdx == -1) buffer[0] = -1;
+        }
+        return buffer[curIdx++];
     }
 }
